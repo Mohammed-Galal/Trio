9715f8f386bf327389800004bb3acc35f0577acf
Mohammed Galal <55014084+Mohammed-Galal@users.noreply.github.com>
2024-09-08 14:05:52 +0200

    Update notes.js

--- notes.js
+++ notes.js
@@ -4,23 +4,37 @@
  * 2. Hooks are only available in Function-Components, and prevent super ($) Usage
  */
 
-const NonFunctionComponent = (
-  <h1 onClick={$.call(function () {})}>count: {$.use("txt")}</h1>
-);
+function handleClick() {
+  // CODE
+  // return true to update UI DOM-Component
+  return true;
+}
+
+let txt = 0;
+
+const JSXComponent = <h1 onClick={handleClick}>count: {txt}</h1>;
 
 function functionComponent(props) {
-  let [count, setCount, countChanged] = useWatcher(0);
-  // setCount (does not) automatically update the state
+  let count = 0;
 
-  const incremenet = () => count++;
+  const incremenet = () => count++ || true;
 
-  useEffect(function () {}, [countChanged]); // before first Paint and Applying Updates
-  useLayoutEffect(function () {}, [countChanged]); // after first Paint and Applying Updates
+  useWatcher(function (track, call) {
+    // false => before first Paint and Applying Updates
+    // true => after first Paint and Applying Updates
+    track([count]) && call(false, logCount);
+  });
+
+  function logCount() {
+    console.log(count);
+  }
+
+  setInterval(useForce(incremenet), 1000);
 
   return (
     <Frag>
       <NonFunctionComponent txt={count()} />
-      <button onClick={useSignal(incremenet)}>{props["propName"]}</button>
+      <button onClick={incremenet}>{use("propName")}</button>
     </Frag>
   );
 }
