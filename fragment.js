10e395eb64612d9b439811bc1250f57f0ae70611
Mohammed Galal <55014084+Mohammed-Galal@users.noreply.github.com>
2024-11-20 14:13:38 +0200

    ...

--- fragment.js
+++ fragment.js
@@ -1,49 +1,35 @@
 const IS_ARRAY = Array.isArray;
+const EMPTY_STR = "";
 const TRASH = new DocumentFragment();
 
 function DOM_FRAG() {
   this.placeholder = document.createTextNode(EMPTY_STR);
   this.frag = document.createDocumentFragment();
-  this.consumed = false;
-  this.cache = new Map();
   this.nodes = [];
+  this.cache = new Map();
 }
 
 export default DOM_FRAG;
 
 const PROTO = DOM_FRAG.prototype;
 
-PROTO.insertNode = function (HTMLNode) {
-  const SELF = this;
-  if (IS_ARRAY(HTMLNode))
-    for (let i = 0; i < HTMLNode.length; ) SELF.insertNode(HTMLNode[i++]);
-  else if (HTMLNode instanceof DOM_FRAG) HTMLNode.appendTo(SELF.frag);
-  else SELF.frag.appendChild(HTMLNode);
-};
-
 PROTO.appendTo = function (containerNode) {
+  // if (IS_ARRAY(HTMLNode))
+  //   for (let i = 0; i < HTMLNode.length; ) SELF.insertNode(HTMLNode[i++]);
+  // else if (HTMLNode instanceof DOM_FRAG) HTMLNode.appendTo(SELF.frag);
+  // else SELF.frag.appendChild(HTMLNode);
+
   containerNode.appendChild(this.frag);
   containerNode.appendChild(this.placeholder);
 };
 
-PROTO.clear = function () {
-  /** Capturing checkpoint before clear proccess.
-   * Object.assign([], domFrag.nodes)
-   * domFrag.clear()
-   */
-  const nodes = this.nodes;
-  for (let i = 0; i < nodes.length; ) TRASH.appendChild(nodes[i++]);
-  this.consumed = false;
+PROTO.insertNode = function (HTMLNode) {
+  const Nodes = this.nodes;
+  Nodes[Nodes.length] = HTMLNode;
 };
 
-PROTO.show = function () {
-  const parent = this.placeholder.parentElement,
-    currDOM = this.nodes,
-    childNodes = this.frag.childNodes;
-
-  Object.assign(currDOM, childNodes);
-  currDOM.length = childNodes.length;
-
-  parent.insertBefore(this.frag, this.placeholder);
-  this.consumed = true;
+PROTO.clear = function () {
+  const Nodes = this.nodes;
+  for (let i = 0; i < Nodes.length; ) TRASH.appendChild(Nodes[i++]);
+  Nodes.length = 0;
 };
