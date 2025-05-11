ded680c7e294f5a211215eea4b1857611b71eef6
Mohammed Galal <55014084+Mohammed-Galal@users.noreply.github.com>
2025-05-12 02:45:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,96 +3,72 @@ const OBJ_KEYS = Object.keys;
 const ASSIGN_OBJ = Object.assign;
 const IS_INT = Number.isInteger;
 const PROTO = Component.prototype;
-
+const PendingUpdates = new Set();
+const ChildrenContainer = new WeakMap();
 const CustomAttrs = {};
-const evExp = /^on[A-Z]/;
+const EventExp = /^on[A-Z]/;
+const EmpyStr = "";
+
+const Effects = {
+  layoutEffects: [],
+  effects: [],
+};
+const ERR = {
+  C: ["Component is not defined", "Component is not a function"],
+  S: ["Script is not defined", "Script is not a function"],
+};
+
+let isUpdating = false;
 
 function Component(C) {
   if (this.constructor !== Component) return new Component(C);
   this.scripts = C.scripts;
   this.components = C.components;
   this.observers = [];
+  this.cachedComponents = new Map();
 }
 
 PROTO.getScript = function (index) {
   return this.scripts[index];
 };
 
-PROTO.renderElement = function (tag, props, children) {
-  const el = document.createElement(tag),
-    registeredEvents = {},
-    ctx = this;
+PROTO.render = function (JSXNode) {
+  const ctx = this;
+  if (JSXNode.constructor !== Array) return createJSXNode(JSXNode, ctx);
 
-  ctx.observers.push(initProps(props, ctx, applyProp));
+  // check if Element has key
+  return (IS_INT(JSXNode[0]) ? renderComponent : createElement)(JSXNode, ctx);
+};
 
-  children &&
-    children.forEach((child) => {
-      const childNode = unNamed(child);
-      if (childNode.constructor !== DocFrag) return el.appendChild(childNode);
-      // dom frag handler
-    });
+PROTO.retrieveComponent = function (c) {
+  if (c && c.constructor !== Object) throw ERR.C[0];
 
-  return el;
+  const ctx = this,
+    DOMProps = c.dom[1];
 
-  function applyProp(name, value) {
-    if (value === undefined) return;
+  if (DOMProps && DOMProps.hasOwnProperty("key")) {
+    const key = IS_INT(DOMProps.key)
+      ? ctx.getScript(DOMProps.key)
+      : DOMProps.key;
 
-    if (evExp.test(name)) {
-      const isRegistered = registeredEvents.hasOwnProperty(name);
-      value.constructor === Function && (registeredEvents[name] = value);
-      if (isRegistered) return;
-      const evType = name.slice(2).toLowerCase();
-      el.addEventListener(evType, (ev) =>
-        registeredEvents[name].call(el, ev, name)
-      );
+    if (!ctx.cachedComponents.has(key)) {
+      const newCtx = new Component(c),
+        registeredComponent = {};
+      ctx.cachedComponents.set(key, registeredComponent);
+      registeredComponent.observers = newCtx.observers;
+      registeredComponent.DOM = createJSXNode(c.dom, newCtx);
     }
-    // ========
-    else if (CustomAttrs.hasOwnProperty(name)) CustomAttrs[name](el, name, val);
-    else if (value.constructor === Object) ASSIGN_OBJ(el[name], value);
-    else if (el.hasOwnProperty(name)) el[name] = value;
-    else el.setAttribute(name, value);
+
+    const targetCtx = ctx.cachedComponents.get(key);
+    targetCtx.observers.forEach((o) => o());
+    return targetCtx.DOM;
   }
+
+  const newCtx = new Component(c);
+  return createJSXNode(c.dom, newCtx);
 };
 
-PROTO.handleChildComponent = function ([index, props, children]) {
-  const component = this,
-    childComponent = component.components[index];
-
-  if (childComponent.constructor === Object)
-    return unNamed(childComponent.dom, new Component(childComponent));
-
-  const observers = component.observers,
-    cachedComponents = new Map(),
-    unNamed_2 = new MyLib(childComponent, children, updateComponent);
-  // =>>  we pass the props to unNamed_2.call method <<= //
-
-  let initializerFunction = unNamed_2.call;
-  if (props) {
-    initializerFunction = initProps(props, component, unNamed_2.call);
-  }
-
-  observers.push(initializerFunction);
-
-  let currentActiveKey;
-
-  return docFrag;
-
-  function updateComponent(resultComponent) {
-    /**
-     * @IF currentActiveKey is not the same as the result key.
-     * 1. retreive component's Element from saved version of the component's key.
-     *    OR render the result if it's not saved yet, then save it by it's key.
-     * 2. replace currentActiveKey accordingly.
-     *
-     * @finally
-     * 1. run unNamed.layoutEffects => unNamed.run("layoutEffects")
-     * 2. run current Component observers
-     * 3. run unNamed.Effects => unNamed.run("Effects")
-     */
-  }
-};
-
-function unNamed(node, ctx) {
+function createJSXNode(node, ctx) {
   if (node === undefined) throw ERR.C[0];
 
   switch (node.constructor) {
@@ -118,6 +94,123 @@ function unNamed(node, ctx) {
   }
 }
 
+function createElement([tag, props, children], ctx) {
+  const el = document.createElement(tag),
+    registeredEvents = {};
+
+  ctx.observers.push(initProps(props, ctx, changedPropsCallback));
+
+  children &&
+    children.forEach((child) => {
+      const childNode = createJSXNode(child);
+      if (childNode.constructor !== DocFrag) return el.appendChild(childNode);
+      // dom frag handler
+    });
+
+  return el;
+
+  function changedPropsCallback(newProps, changedProps) {
+    changedProps.forEach((k) => applyProp(k, newProps[k]));
+  }
+
+  function applyProp(name, value) {
+    if (value === undefined) return;
+
+    if (EventExp.test(name)) {
+      const isRegistered = registeredEvents.hasOwnProperty(name);
+      value.constructor === Function && (registeredEvents[name] = value);
+      if (isRegistered) return;
+      const evType = name.slice(2).toLowerCase();
+      el.addEventListener(evType, (ev) =>
+        registeredEvents[name].call(el, ev, name)
+      );
+    }
+    // ========
+    else if (CustomAttrs.hasOwnProperty(name)) CustomAttrs[name](el, name, val);
+    else if (value.constructor === Object) ASSIGN_OBJ(el[name], value);
+    else if (el.hasOwnProperty(name)) el[name] = value;
+    else el.setAttribute(name, value);
+  }
+}
+
+function renderComponent([index, props, children], ctx) {
+  const component = ctx.components[index];
+
+  if (component.constructor === Object)
+    return createJSXNode(component.dom, new Component(component));
+
+  const childrenAsProp = new ChildrenData();
+  if (children) {
+    const childrenRef = Symbol("childrenRef"),
+      childrenData = { children, ctx };
+    childrenAsProp.ref = childrenRef;
+    childrenAsProp.use = function (fn) {
+      // check if fn is a function, otherwise throw Error
+      childrenData.use ||= fn;
+    };
+    ChildrenContainer.set(childrenRef, childrenData);
+  }
+  Object.freeze(childrenAsProp);
+
+  const docFrag = new DocFrag(),
+    unNamed_2 = new MyLib(component, childrenAsProp, updateComponent),
+    initializerFN = props ? initProps(props, ctx, pendUpdate) : pendUpdate;
+
+  // =>>  we pass the props to unNamed_2.call method <<= //
+  let currentActiveKey;
+  const cachedComponents = new Map();
+
+  initializerFN();
+  return docFrag;
+
+  function pendUpdate(props) {
+    isUpdating
+      ? PendingUpdates.add(() => unNamed_2.call(props))
+      : unNamed_2.call(props);
+  }
+
+  function updateComponent(resultComponent) {
+    unNamed_2.run("layoutEffects");
+
+    switch (resultComponent && resultComponent.constructor) {
+      case Object:
+        const key = resultComponent.key;
+        let newComponent;
+        if (currentActiveKey !== key) {
+          if (!cachedComponents.has(key)) {
+            newComponent = new Component(resultComponent);
+            const cache = {
+              observers: newComponent.observers,
+              DOM: createJSXNode(resultComponent.dom, newComponent),
+            };
+            cachedComponents.set(k, cache);
+          }
+          newComponent = cachedComponents.get(key);
+          docFrag.replaceChildren(newComponent.DOM);
+        }
+        newComponent.observers.forEach((fn) => fn());
+        currentActiveKey = key;
+        break;
+
+      case Array:
+        docFrag.clear();
+        resultComponent.forEach(iteratorHandler);
+        function iteratorHandler(c) {
+          docFrag.insertNode(ctx.retrieveComponent(c));
+        }
+        break;
+
+      case Number:
+        resultComponent = EmpyStr + resultComponent;
+
+      default:
+        docFrag.replaceChildren(EmpyStr + (resultComponent || EmpyStr));
+    }
+
+    unNamed_2.run("effects");
+  }
+}
+
 function initProps(props, ctx, propChangeCallback) {
   const hasSpreadedProps = props.assign.length > 0,
     inlineProps = props.inline,
@@ -130,34 +223,30 @@ function initProps(props, ctx, propChangeCallback) {
   let targetProps = dynamicProps;
 
   if (hasSpreadedProps) ctx.observers.push(updateHandler);
-  else if (dynamicProps.length) {
+  else {
     const dynamicKeys = propsEntries.filter((key) => IS_INT(inlineProps[key]));
     Object.assign(dynamicProps, dynamicKeys);
-    ctx.observers.push(updateHandler);
   }
 
   return updateHandler;
 
   function updateHandler() {
     const scripts = ctx.scripts;
-    props.assign.forEach((objRef) =>
-      ASSIGN_OBJ(propsContainer, scripts[objRef])
-    );
-
+    changedProps.length = 0;
     if (hasSpreadedProps) {
-      propsEntries.forEach(propHandler);
+      props.assign.forEach((objRef) =>
+        ASSIGN_OBJ(propsContainer, scripts[objRef])
+      );
+      propsEntries.forEach(checkProp);
       targetProps = OBJ_KEYS(propsContainer);
     }
+    targetProps.forEach(checkProp);
 
-    targetProps.forEach(propHandler);
-    changedProps.forEach((key) => propChangeCallback(key, propsContainer[key]));
-    changedProps.length = 0;
+    propChangeCallback && propChangeCallback(propsContainer, targetProps);
     return propsContainer;
   }
 
-  function propHandler(prop) {
-    // review Prop
-    // invoke callback function whenever we detect prop change
+  function checkProp(prop) {
     const isDynamicProp = IS_INT(inlineProps[prop]),
       currentPropValue = isDynamicProp
         ? ctx.getScript(prop)
@@ -170,10 +259,7 @@ function initProps(props, ctx, propChangeCallback) {
 
 function DocFrag() {
   if (this.constructor !== DocFrag) return new DocFrag();
-
-  let currentActiveKey;
-  const placeholder = document.createTextNode(""),
-    cachedElements = new Map();
-
-  Object.assign(this, { placeholder });
+  this.placeholder = document.createTextNode("");
 }
+
+function ChildrenData() {}
